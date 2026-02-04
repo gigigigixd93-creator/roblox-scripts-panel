984f0d771cd858551b23d7f5f62e96167d7d461b
gigigigixd93-creator <gigigigixd93@gmail.com>
2026-02-04 17:48:31 -0600

    Refactor Dex Explorer entry in scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -1,3 +1,3 @@
 return {
-    ["Dex Explorer"] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/infyiff/backup/main/dex.lua'))()"
+    ["Dex Explorer"]={code="loadstring(game:HttpGet('https://raw.githubusercontent.com/infyiff/backup/main/dex.lua'))()",author="Admin"},
 }
